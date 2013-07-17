c4bd39488b9981f0056eb8f1a7d935891e5470db
Nicholas Wargnier <nwargnier@gmail.com>
2013-07-17 10:25:36 -0700

    Set up environment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeedbackOverflow::Application.config.secret_token = 'fee79f96c61d4d84340bf62f158b3b0696672dcd59abf0a5508a161fe3300d46d5c136690934cb636e32b370d81aa1e015c4bfb333e591a133c34617f3ca3323'
