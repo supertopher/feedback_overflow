9f4693a983d7596eb19425bc7d8d151ff7a315dc
Nicholas Wargnier <nwargnier@gmail.com>
2013-07-17 11:42:11 -0700

    added user controller and views and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,34 @@
 class User < ActiveRecord::Base
+  attr_accessible :username, :email, :password
   has_secure_password
   has_many :topics
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :username, presence: true,
+                       uniqueness: true
+  validates :email, presence: true,
+                    uniqueness: true,
+                    format: {with: VALID_EMAIL_REGEX}
+
+  validates :password, presence: true,
+                       length: { minimum: 6}
+
+  validates_confirmation_of :password
+  before_save {self.email = email.downcase}
+  before_create :create_remember_token
+
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
+
