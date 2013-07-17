441dd1a183980ae89b1835addf078ee9347a9692
Nicholas Wargnier <nwargnier@gmail.com>
2013-07-17 12:00:21 -0700

    Fixed form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,10 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
 
+  def create
+    # @user = User.new(params[:user])
+    p params
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ FeedbackOverflow::Application.routes.draw do
 
   root to: 'topics#index'
   resource :topics, except: [:show]
-
+  resources :users
   get '/topic/:stub' => 'topics#show', :as => 'topic'
+
 end
