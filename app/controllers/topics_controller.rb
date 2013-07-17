79a06a67107395b4d83068696b84ecbdc8b98272
Apprentice <apprentice@devbootcamp.com>
2013-07-17 11:44:54 -0700

    Added slug to topic model, added topic views.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,4 +2,12 @@ class TopicsController < ApplicationController
   def index
     @topics = Topic.all
   end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def show
+    @topic = Topic.find_by_slug(params[:slug])
+  end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,11 @@
 class Topic < ActiveRecord::Base
+  attr_accessible :title, :content
   belongs_to :user
+
+  before_create :to_slug
+
+  private
+  def to_slug
+    self.slug = self.title.downcase.gsub(/[^a-z1-9]+/, '-').chomp('-')
+  end
 end
